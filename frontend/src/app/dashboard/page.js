fb0d0a7be125e23083254d90075fc49aff28608d
Sina <naseri.sina@hotmail.com>
2025-02-04 15:27:57 +0000

    added new folders and components

---
+++ frontend/src/app/dashboard/page.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from "../../components/Navbar";
+
+const page = () => {
+  return (
+    <div>
+      dashboard
+      <Navbar/>
+    </div>    
+  )
+}
+
+export default page
---
+++ frontend/src/app/login/page.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      This is the login page 
+    </div>
+  )
+}
+
+export default page
