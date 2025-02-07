9bd6b26d5658d1a4ec250cc01f5129bda6ab61dd
Sina <naseri.sina@hotmail.com>
2025-02-07 14:59:49 +0000

    fixed the controller for sina

--- backend/controller.js
+++ backend/controller.js
@@ -1,7 +1,7 @@
-
-export async function Validate(req, res) {
-  console.log("hitting this function")
+async function Validate(req, res) {
+  console.log("hitting this function");
   res
     .status(200)
     .send({ message: "Welcome to the dashboard!", user: req.user });
 }
+module.exports = { Validate };
